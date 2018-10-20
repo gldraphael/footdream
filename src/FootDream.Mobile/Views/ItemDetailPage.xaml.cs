4875f7cb83f9bb6a85f6a61abb104cc865044181
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-10-20 12:48:01 +0530

    Create a new Xamarin Forms project

---
+++ src/FootDream.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using FootDream.Mobile.Models;
+using FootDream.Mobile.ViewModels;
+
+namespace FootDream.Mobile.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ItemDetailPage : ContentPage
+	{
+		ItemDetailViewModel viewModel;
+
+		public ItemDetailPage(ItemDetailViewModel viewModel)
+		{
+			InitializeComponent();
+
+			BindingContext = this.viewModel = viewModel;
+		}
+
+		public ItemDetailPage()
+		{
+			InitializeComponent();
+
+			var item = new Item
+			{
+				Text = "Item 1",
+				Description = "This is an item description."
+			};
+
+			viewModel = new ItemDetailViewModel(item);
+			BindingContext = viewModel;
+		}
+	}
+}
