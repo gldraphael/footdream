375dc49ab12938af268a6849dbdc378dc05128eb
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-10-20 13:49:44 +0530

    Setup an /api/news action that returns news from goal.com

---
+++ src/FootDream.Web/Controllers/NewsController.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using FootDream.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootDream.Web.Controllers
+{
+	[ApiController, Route("api/[controller]")]
+	public class NewsController : Controller
+	{
+		[HttpGet("")]
+		public async Task<IActionResult> GetNews([FromServices]INewsService news) => 
+			Ok(await news.GetAsync());
+	}
+}
---
+++ src/FootDream.Web/Services/SimpleNewsService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CodeHollow.FeedReader;
+using FootDream.Web.Models;
+using Mapster;
+
+namespace FootDream.Web.Services
+{
+	public class SimpleNewsService : INewsService
+	{
+
+		const string feedUrl = "http://www.goal.com/en-us/feeds/news?fmt=rss";
+
+		public async Task<IList<NewsArticle>> GetAsync()
+		{
+			var feed = await FeedReader.ReadAsync(feedUrl);
+			return feed.Items.Adapt<List<NewsArticle>>().AsReadOnly();
+		}
+	}
+}
